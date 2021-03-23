Better Module inheritance. Fixed gradients with mean. Separated sampler.

examples/mnist_baseline_classifier.py
@@ -1,4 +1,3 @@
-import qaml
 import torch
 
 import matplotlib.pyplot as plt
@@ -11,7 +10,7 @@ import torchvision.transforms as torch_transforms
 N, D_in, H, D_out = 64, 784, 128, 10
 
 ################################# Hyperparameters ##############################
-EPOCHS = 10
+EPOCHS = 20
 BATCH_SIZE = 64
 # Stochastic Gradient Descent
 learning_rate = 1e-3
@@ -30,62 +29,22 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE,
 test_dataset = torch_datasets.MNIST(root='./data/', train=False,
                                     transform=torch_transforms.ToTensor(),
                                     download=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE)
-
-
-################################# Model Definition #############################
-# Specify model with dimensions
-rbm = qaml.nn.RBM(D_in, H)
-
-# Set up optimizer
-optimizer = torch.optim.SGD(rbm.parameters(), lr=learning_rate,
-                                              weight_decay=weight_decay,
-                                              momentum=momentum)
-# Set up training mechanisms
-CD = qaml.autograd.ConstrastiveDivergence()
+test_loader = torch.utils.data.DataLoader(test_dataset)
 
 ################################## Model Training ##############################
-# Set the model to training mode
-rbm.train()
-for t in range(EPOCHS):
-    for v_batch, labels_batch in train_loader:
-        # Negative LogLikehood from Contrastive divergence
-        err = CD.apply(v_batch.view(len(v_batch),D_in),*rbm.parameters())
-
-        # Before the backward pass, use the optimizer object to zero all of the
-        # gradients for the variables it will update.
-        optimizer.zero_grad()
-
-        # Backward pass: compute gradient of the likelihood with respect to
-        # model parameters
-        err.backward()
-
-        # Calling the step function on an Optimizer makes an update to its
-        # parameters
-        optimizer.step()
-    print(f"Epoch {t} Reconstruction Error = {err.item()}")
-# Set the model to evaluation mode
-rbm.eval()
-plt.matshow(rbm.bv.detach().view(28, 28))
-
-# Save model
-# torch.save(rbm)
-
-
-model = torch.nn.Sequential(rbm,
-                            torch.nn.Linear(H,D_out),
-                            torch.nn.ReLU())
+model = torch.nn.Sequential(torch.nn.Linear(D_in, H),
+                            torch.nn.ReLU(),
+                            torch.nn.Linear(H, D_out),)
 loss_fn = torch.nn.MSELoss(reduction='sum')
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-for t in range(20):
+for t in range(EPOCHS):
     for v_batch, labels_batch in train_loader:
         # Forward pass: compute predicted y by passing x to the model.
         y_pred = model(v_batch.view(len(v_batch),D_in))
 
         # Compute and print loss.
         loss = loss_fn(y_pred,  torch.nn.functional.one_hot(labels_batch,10)*1.0)
-        print(t, loss.item())
 
         # Before the backward pass, use the optimizer object to zero all of the
         # gradients for the Tensors it will update (which are the learnable weights
@@ -97,11 +56,21 @@ for t in range(20):
 
         # Calling the step function on an Optimizer makes an update to its parameters
         optimizer.step()
+    print(f"Epoch {t} Loss = {loss.item()}")
 
-test = test_loader.dataset.data[60].view(1,784)*1.0
+count = 0
+for test_data, test_label in test_loader:
+    label_pred = model(test_data.view(1,D_in)).argmax()
+    if label_pred == test_label:
+        count+=1
+count
+
+test = test_loader.dataset.data[77].view(1,784)*1.0
 plt.matshow(test.view(28, 28))
 
 loss
+
+model(test)
 
 model(test).argmax()
 

qaml/autograd.py
@@ -1,69 +1,32 @@
 import torch
 import torch.nn.functional as F
 
-# class PersistentConstrastiveDivergence(torch.autograd.Function)
-#    v_k = 0
-    # @staticmethod
-    # def forward(ctx, input, bias_v, bias_h, weights):
-
-
 class ConstrastiveDivergence(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, input, bias_v, bias_h, weights):
-
-        # Positive Phase
-        p_h_0 = torch.sigmoid(F.linear(input, weights, bias_h))
-
-        # Negative Phase
-        p_h_k = p_h_0.clone()
-        for _ in range(2):
-            p_v_k = torch.sigmoid(F.linear(p_h_k, weights.t(), bias_v))
-            v_k = p_v_k.bernoulli()
-            p_h_k = torch.sigmoid(F.linear(v_k, weights, bias_h))
-
+    def forward(ctx, pos_phase, neg_phase, bias_v, bias_h, weights):
+        v0, prob_h0 = pos_phase
+        prob_vk, prob_hk = neg_phase
         # Value for gradient
-        ctx.save_for_backward(input, p_h_0, v_k, p_h_k)
-        return torch.nn.functional.mse_loss(input, p_v_k, reduction='sum')
+        ctx.save_for_backward(v0, prob_h0, prob_vk, prob_hk)
+        return torch.nn.functional.mse_loss(v0, prob_vk, reduction='sum')
 
 
     @staticmethod
     def backward(ctx, grad_output):
-        # Retrueve positive and negative phase values
-        v_0, p_h_0, v_k, p_h_k = ctx.saved_tensors
+        # Retrieve positive and negative phase values
+        v0, prob_h0, prob_vk, prob_hk = ctx.saved_tensors
 
         # for j = 1,...,m do
-        #     v_j^{(0)} - v_j^{(k)}
-        v_grad = -(v_0 - v_k)
+        #     \Delta a_j += v_j^{0} - v_j^{k}
+        v_grad = -grad_output*torch.mean(v0 - prob_vk, dim=0)
 
         # for i = 1,...,n do
-        #     p(H_i = 1 | v^{(0)}) - p(H_i = 1 | v^{(k)})
-        h_grad = -(p_h_0 - p_h_k)
+        #     \Delta b_i += p(H_i = 1 | v^{0}) - p(H_i = 1 | v^{k})
+        h_grad = -grad_output*torch.mean(prob_h0 - prob_hk, dim=0)
 
         # for i = 1,...,n, j = 1,...,m do
-        #     p(H_i = 1 | v^{(0)})*v_j^{(0)} - p(H_i = 1 | v^{(k)})*v_j^{(k)}
-        W_grad = -(p_h_0.t().mm(v_0) - p_h_k.t().mm(v_k))
+        #     \Delta w_{ij} += p(H_i=1|v^{0})*v_j^{0} - p(H_i=1|v^{k})*v_j^{k}
+        W_grad = -grad_output*(prob_h0.t().mm(v0) - prob_hk.t().mm(prob_vk))/len(v0)
 
-        return None, v_grad, h_grad, W_grad
-
-
-"""
->>> from torchviz import make_dot
->>>
->>> N = 4
->>> M = 8
->>> # Create random Tensors to hold input and output
->>> x = torch.randn(10,N)
->>>
->>> # Create random Tensors for weights.
->>> v = torch.randn(1,N, requires_grad=True)
->>> h = torch.randn(1,M, requires_grad=True)
->>> W = torch.randn(M, N, requires_grad=True)
->>>
->>> CD = ConstrastiveDivergence()
->>> ll = CD.apply(x,v,h,W)
->>>
->>> ll.backward(retain_graph=True)
->>>
->>> make_dot(ll, params= {'v<sub>i</sub>':v,'h<sub>j</sub>':h,'w<sub>ij</sub>':W})
-"""
+        return None, None, v_grad, h_grad, W_grad

qaml/nn.py
@@ -4,27 +4,72 @@ import torch.nn.functional as F
 class BoltzmannMachine(torch.nn.Module):
     r"""Boltzmann Machine.
 
-    Args:
-        n_vis (int, optional): The size of visible layer
-        n_hid (int, optional): The size of hidden layer
     """
 
-    def __init__(self, V_in, H_out):
+    def __init__(self):
         super(BM, self).__init__()
-        # Visible linear bias
-        self.bv = torch.nn.Parameter(torch.ones(V_in)*0.5, requires_grad=True)
-        # Hidden linear bias
-        self.bh = torch.nn.Parameter(torch.zeros(H_out), requires_grad=True)
-        # Visible-Hidden quadratic bias
-        self.W = torch.nn.Parameter(torch.randn(H_out, V_in)*0.1, requires_grad=True) 
+        self._matrix = None #TODO
 
-    def sample(self):
+    @property
+    def matrix(self):
+        #TODO
+        return self._matrix
+
+    def energy(self, visible, hidden):
+        return
+
+    def free_energy(self, input, beta=1.0):
         return
 
     def forward(self, input):
         return
 
 BM = BoltzmannMachine
+
+class RestrictedBoltzmannMachine(BoltzmannMachine):
+    r""" Restricted Boltzmann Machine.
+
+    Args:
+        V (int): The size of visible layer
+        H (int): The size of hidden layer
+    """
+
+    V : int # Visible nodes
+    H : int # Hidden nodes
+
+    def __init__(self, V, H):
+        super(RestrictedBoltzmannMachine, self).__init__()
+        self.V = V
+        self.H = H
+        # Visible linear bias
+        self.bv = torch.nn.Parameter(torch.ones(V)*0.5, requires_grad=True)
+        # Hidden linear bias
+        self.bh = torch.nn.Parameter(torch.zeros(H), requires_grad=True)
+        # Visible-Hidden quadratic bias
+        self.W = torch.nn.Parameter(torch.randn(H, V)*0.1, requires_grad=True)
+
+    def generate(self, hidden=None, k=1):
+        p_h_k = hidden.clone()
+        for _ in range(k):
+            p_v_k = torch.sigmoid(F.linear(p_h_k, self.W.t(), self.bv))
+            v_k = p_v_k.bernoulli()
+            p_h_k = torch.sigmoid(F.linear(v_k, self.W, self.bh))
+        return v_k
+
+    def forward(self, input):
+        return torch.sigmoid(F.linear(input, self.W, self.bh))
+
+    def free_energy(self, input, beta=1.0):
+        """ E(v) = -a \cdot v - \sum_j(\log(1+\exp(b+vW)))_j """
+
+        visible_contribution = torch.dot(self.bv, input)
+
+        vW_h = F.linear(input, self.W, self.bh)
+        hidden_contribution = torch.sum(F.softplus(vW_h,beta))
+
+        return -(visible_contribution + hidden_contribution)
+
+RBM = RestrictedBoltzmannMachine
 
 class LimitedBoltzmannMachine(BoltzmannMachine):
 
@@ -35,37 +80,3 @@ class LimitedBoltzmannMachine(BoltzmannMachine):
         pass
 
 LBM = LimitedBoltzmannMachine
-
-class RestrictedBoltzmannMachine(BoltzmannMachine):
-
-
-    def forward(self, input, k=1):
-        if self.training:
-
-            for _ in range(k):
-                pH_v = torch.sigmoid(F.linear(input, self.W, self.bh))
-                pV_h = torch.sigmoid(F.linear(pH_v, self.W.t(), self.bv))
-                input.data = pV_h.bernoulli()
-            return pH_v
-
-        else:
-            return torch.sigmoid(F.linear(input, self.W, self.bh))
-
-RBM = RestrictedBoltzmannMachine
-
-class QuantumAssistedRestrictedBoltzmannMachine(RestrictedBoltzmannMachine):
-
-
-    def forward(self, input, k=1):
-        if self.training:
-
-            for _ in range(k):
-                pH_v = torch.sigmoid(F.linear(input, self.W, self.bh))
-                pV_h = torch.sigmoid(F.linear(pH_v, self.W.t(), self.bv))
-                input.data = pV_h.bernoulli()
-            return pH_v
-
-        else:
-            return torch.sigmoid(F.linear(input, self.W, self.bh))
-
-QARBM = QuantumAssistedRestrictedBoltzmannMachine