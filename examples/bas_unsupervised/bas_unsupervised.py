Fixed energy and BQM signs. New examples CD

examples/bas_unsupervised/bas_unsupervised.py
@@ -10,8 +10,8 @@ import torchvision.transforms as torch_transforms
 ################################# Hyperparameters ##############################
 SHAPE = (4,4)
 EPOCHS = 20
-SAMPLES = 1000
-BATCH_SIZE = 250
+SAMPLES = 250
+BATCH_SIZE = 1
 # Stochastic Gradient Descent
 learning_rate = 0.1
 weight_decay = 1e-4
@@ -43,7 +43,7 @@ optimizer = torch.optim.SGD(rbm.parameters(), lr=learning_rate,
 sampler = qaml.sampler.GibbsNetworkSampler(rbm)
 CD = qaml.autograd.ConstrastiveDivergence()
 sa_sampler = qaml.sampler.SimulatedAnnealingNetworkSampler(rbm)
-CD = qaml.autograd.SampleBasedConstrastiveDivergence()
+# CD = qaml.autograd.SampleBasedConstrastiveDivergence()
 
 ################################## Model Training ##############################
 # Set the model to training mode
@@ -79,7 +79,7 @@ for t in range(EPOCHS):
 plt.plot(err_log)
 plt.ylabel("Reconstruction Error")
 plt.xlabel("Epoch")
-plt.savefig("err_log.png")
+plt.savefig("gibbs_err_log.png")
 
 # Set the model to evaluation mode
 rbm.eval()
@@ -95,12 +95,19 @@ noise_energies = []
 for _ in range(len(train_dataset)):
     noise_energies.append(rbm.free_energy(torch.rand(rbm.V).bernoulli()).item())
 
-plt.hist(noise_energies,label="Random",bins=100)
-plt.hist(data_energies, label="Data",bins=100)
+model_energies = []
+for _ in range(len(train_dataset)):
+    v,h = sa_sampler(num_reads=1)
+    model_energies.append(rbm.energy(v.flatten(),h.flatten()).item())
+
+hist_kwargs = {'alpha':0.5,'density':True,'bins':100}
+plt.hist(noise_energies,label="Random", **hist_kwargs)
+plt.hist(data_energies, label="Data", **hist_kwargs)
+plt.hist(model_energies, label="Model", **hist_kwargs)
 plt.ylabel("Count")
 plt.xlabel("Energy")
 plt.legend()
-plt.savefig("sa_energies.png")
+plt.savefig("gibbs_energies.png")
 
 ################################## VISUALIZE ###################################
 fig,axs = plt.subplots(3,5)

qaml/autograd.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn.functional as F
 
 class ConstrastiveDivergence(torch.autograd.Function):
 
@@ -7,7 +6,7 @@ class ConstrastiveDivergence(torch.autograd.Function):
     def forward(ctx, pos_phase, neg_phase, bias_v, bias_h, weights):
         v0, prob_h0 = pos_phase
         prob_vk, prob_hk = neg_phase
-        # Value for gradient
+        # Values for gradient
         ctx.save_for_backward(v0, prob_h0, prob_vk, prob_hk)
         return torch.nn.functional.mse_loss(v0, prob_vk, reduction='sum')
 
@@ -16,6 +15,9 @@ class ConstrastiveDivergence(torch.autograd.Function):
     def backward(ctx, grad_output):
         # Retrieve positive and negative phase values
         v0, prob_h0, prob_vk, prob_hk = ctx.saved_tensors
+
+        # Bath size
+        S = len(v0)
 
         # for j = 1,...,m do
         #     \Delta a_j += v_j^{0} - v_j^{k}
@@ -27,6 +29,37 @@ class ConstrastiveDivergence(torch.autograd.Function):
 
         # for i = 1,...,n, j = 1,...,m do
         #     \Delta w_{ij} += p(H_i=1|v^{0})*v_j^{0} - p(H_i=1|v^{k})*v_j^{k}
-        W_grad = -grad_output*(prob_h0.t().mm(v0) - prob_hk.t().mm(prob_vk))/len(v0)
+        W_grad = -grad_output*(prob_h0.t().mm(v0) - prob_hk.t().mm(prob_vk))/S
+
+        return None, None, v_grad, h_grad, W_grad
+
+class SampleBasedConstrastiveDivergence(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, pos_phase, neg_phase, bias_v, bias_h, weights):
+        samples_v0, samples_h0 = pos_phase
+        samples_vk, samples_hk = neg_phase
+
+        expect_k = torch.mean(samples_vk, dim=0)
+        expect_0 = torch.mean(samples_v0, dim=0)
+
+        # Values for gradient
+        ctx.save_for_backward(samples_v0, samples_h0, samples_vk, samples_hk)
+        return torch.nn.functional.l1_loss(expect_k, expect_0, reduction='sum')
+
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # Retrieve positive and negative phase values
+        samples_v0, samples_h0, samples_vk, samples_hk = ctx.saved_tensors
+
+        #Batch size
+        S = len(samples_v0)
+
+        v_grad = -grad_output*(torch.mean(samples_v0, dim=0) - torch.mean(samples_vk, dim=0))
+
+        h_grad = -grad_output*(torch.mean(samples_h0,dim=0) - torch.mean(samples_hk, dim=0))
+
+        W_grad = -grad_output*(samples_h0.t().mm(samples_v0) - samples_hk.t().mm(samples_vk))/S
 
         return None, None, v_grad, h_grad, W_grad

qaml/nn.py
@@ -56,8 +56,8 @@ class RestrictedBoltzmannMachine(BoltzmannMachine):
         return torch.sigmoid(F.linear(input, self.W, self.bh))
 
     def energy(self, visible, hidden):
-        linear = torch.dot(visible, self.bv.T) + torch.dot(hidden, self.bh.T)
-        quadratic = torch.dot(torch.inner(visible, self.W), hidden)
+        linear = -torch.dot(visible, self.bv.T) + torch.dot(hidden, self.bh.T)
+        quadratic = -torch.dot(torch.inner(visible, self.W), hidden)
         return linear + quadratic
 
     def free_energy(self, input, beta=1.0):

qaml/sampler.py
@@ -1,7 +1,7 @@
 import torch
 import dimod
 import warnings
-import torch.nn.functional as F
+import dwave.system
 
 class NetworkSampler(torch.nn.Module):
     def __init__(self, model):
@@ -19,11 +19,11 @@ class NetworkSampler(torch.nn.Module):
         bias_v = self.model.bv.data.numpy()
         bias_h = self.model.bh.data.numpy()
         W = self.model.W.data.numpy()
-        lin_V = {bv: bias for bv,bias in enumerate(bias_v)}
-        lin_H = {bh: bias for bh,bias in enumerate(bias_h)}
+        lin_V = {bv: -bias for bv,bias in enumerate(bias_v)}
+        lin_H = {bh: -bias for bh,bias in enumerate(bias_h)}
 
         linear = {**lin_V,**{self.model.V+j:bh for j,bh in lin_H.items()}}
-        quadratic = {(i,self.model.V+j):W[j][i] for j in lin_H for i in lin_V}
+        quadratic = {(i,self.model.V+j):-W[j][i] for j in lin_H for i in lin_V}
 
         bqm = dimod.BinaryQuadraticModel(linear,quadratic,'BINARY')
         return bqm
@@ -90,26 +90,37 @@ class GibbsNetworkSampler(NetworkSampler):
 
 class SimulatedAnnealingNetworkSampler(NetworkSampler,dimod.SimulatedAnnealingSampler):
 
-    bqm = None
-
+    sa_kwargs = {"num_sweeps":10}
 
     def __init__(self, model):
         super(SimulatedAnnealingNetworkSampler, self).__init__(model)
 
-    def forward(self, num_samples=1, **kwargs):
+    def forward(self, num_reads=1, **kwargs):
         bqm = self.binary_quadratic_model
+        self.sa_kwargs.update(kwargs)
+        sampleset = self.sample(bqm,num_reads=num_reads,**self.sa_kwargs)
 
-        samples = self.sample(bqm,**kwargs)
+        sampletensor = torch.Tensor(sampleset.record.sample)
+        samples_v,samples_h = sampletensor.split([self.model.V,self.model.H],1)
 
-        return samples
+        return samples_v, samples_h
+
+        # expect_values = torch.Tensor(sampleset.record.sample).mean(axis=0)
+        # expect_v, expect_h = expect_values.split([self.model.V,self.model.H])
+        #
+        # return expect_v.repeat((num_reads,1)), expect_h.repeat((num_reads,1))
 
 class QuantumAssistedNetworkSampler(NetworkSampler,dwave.system.DWaveSampler):
+
+    dw_kwargs = {'answer_mode':'raw',
+                 'auto_scale':True}
+
     def __init__(self, model):
         super(QuantumAssistedNetworkSampler, self).__init__(model)
 
-    def forward(self):
+    def forward(self,**kwargs):
         bqm = self.binary_quadratic_model
-
-        samples = self.sample(bqm,**kwargs)
+        self.dw_kwargs.update(kwargs)
+        samples = self.sample(bqm,**self.dw_kwargs)
 
         return samples